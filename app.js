c98017aea2737af34d05aa14e91ca5587fead80d
narutosi77 <ceverianojessi@gmail.com>
2025-06-01 17:35:45 -0600

    probando el commit para los archivos

---
+++ app.js
@@ -0,0 +1,15 @@
+// app.js
+const http = require('http');
+
+const hostname = '0.0.0.0';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('¡Hola desde Docker!\n');
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Servidor corriendo en http://${hostname}:${port}/`);
+});
