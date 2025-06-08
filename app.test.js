32d080250434e04fc8b5ba79238df6f6b97dceca
narutosi77 <ceverianojessi@gmail.com>
2025-06-08 17:01:18 -0600

    Implementar nueva función de saludo y añadir pruebas 2.0

--- app.test.js
+++ app.test.js
@@ -1,23 +1,21 @@
-// app.test.js
-const { greet } = require('./app'); // Importa la función a probar
+
+const { greet } = require('./app'); 
 
 describe('greet function', () => {
-  // Prueba para verificar el comportamiento cuando no se proporciona ningún nombre.
+  
   test('should return "¡Hola desde Docker!" when no name is provided', () => {
-    expect(greet()).toBe('¡Hola desde Docker!\n');
+    
+    expect(greet()).toBe('¡Hola desde Docker!');
   });
 
-  // Prueba para verificar el comportamiento cuando se proporciona un nombre.
-  // La descripción se ha ajustado para ser más genérica, ya que prueba con múltiples nombres.
   test('should return "¡Hola, [nombre] desde Docker!" when a name is provided', () => {
+    
     expect(greet('Mundo')).toBe('¡Hola, Mundo desde Docker!');
     expect(greet('Alice')).toBe('¡Hola, Alice desde Docker!');
-    // Si la función `greet` tiene un comportamiento especial para 'ilyyessy', se podría añadir un test específico para ese caso.
-    // expect(greet('ilyyessy')).toBe('¡Hola, ilyyessy desde Docker!\n');
   });
 
-  // Prueba para verificar el comportamiento cuando se proporciona una cadena vacía como nombre.
   test('should handle empty string as name', () => {
-    expect(greet('')).toBe('¡Hola desde Docker!\n'); // Se espera el comportamiento por defecto si el nombre es una cadena vacía
+    
+    expect(greet('')).toBe('¡Hola desde Docker!');
   });
 });
